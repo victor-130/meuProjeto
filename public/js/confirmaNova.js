7dfd4d1f573755ef96229416f5cdfbdeac05b52d
andra <andravictor11@gmail.com>
2024-09-19 19:41:16 -0300

    atualização 2.2

---
+++ public/js/confirmaNova.js
@@ -0,0 +1,25 @@
+/*const form = {
+    codigo: () => document.getElementsByName('codigo')[0],
+    produto: () => document.getElementsByName('produto')[0],
+    modelo: () => document.getElementsByName('modelo')[0],
+    marca: () => document.getElementsByName('marca')[0],
+    quantidade: () => document.getElementsByName('quantidade')[0],
+    valorc: () => document.getElementsByName('valorc')[0],
+    valorv: () => document.getElementsByName('valorv')[0]
+}
+function confirmaNova(){
+    const confirmaCodigo =  `Código: ${form.codigo().value}\n`
+    const confirmaProduto = `Produto: ${form.produto().value}\n`
+    const confirmaModelo = `Modelo: ${form.modelo().value}\n`
+    const confirmaMarca = `Marca: ${form.marca().value}\n`
+    const confirmaQuantidade = `Quantidade: ${form.quantidade().value}\n`
+    const confirmavalorc = `Valor de Compra: ${form.valorc().value}      `
+    const confirmaValorv = `Valor de venda: ${form.valorv().value}\n`
+
+    return confirm(confirmaCodigo+confirmaProduto+confirmaModelo+confirmaMarca+confirmaQuantidade+confirmavalorc+confirmaValorv)
+}
+function toUpper(){
+    const input = document.getElementById('idproduto').value
+    input = input.toUpperCase()
+    console.log(input)
+}*/
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const bodyParser = require("body-parser")
 const express = require("express")
 const app = express()
 const {engine} =  require('express-handlebars')
-
 const path = require('path')
 
 const database = require('./src/config/database');
@@ -11,6 +10,7 @@ const { Sequelize } = require("sequelize")
 const Op = Sequelize.Op
 
 // config
+    
     //Template Engine
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -18,18 +18,20 @@ const Op = Sequelize.Op
     app.engine('handlebars', engine({defaultLayout: 'main',
         runtimeOptions: {
             allowProtoPropertiesByDefault: true,
-            allowProtoMethodsByDefault: true}
-    }))
+            allowProtoMethodsByDefault: true},
+}))
     app.set('view engine', 'handlebars')
     app.use(express.static(path.join(__dirname,'public')))
 //Rotas
 app.get('/addpeca/novapeca', (req,res) => {
     res.render('addpeca/novapeca')
 })
-app.post('/addpeca/nova',  (req,res) => {
-    try{
- 
-        const novoProduto =  Produto.create({
+app.post('/addpeca/nova', async (req,res) => {
+    try{           
+        /*Produto.sync({ alter: true }).then(() => {
+        console.log('Tabela "produto" sincronizada');      
+        }); */ 
+        const novoProduto = await  Produto.create({
             codigo: req.body.codigo,
             produto: req.body.produto,
             modelo: req.body.modelo,
@@ -38,28 +40,21 @@ app.post('/addpeca/nova',  (req,res) => {
             valorc: req.body.valorc,
             valorv: req.body.valorv
         })
-              
-        Produto.sync({ alter: true }).then(() => {
-        console.log('Tabela "produto" sincronizada');      
-        });     
+
         res.redirect('/')
     }catch(err){
         res.status(500).json({err: 'erro ao criar produto'})
         console.log(err)
     }
 })
+
 app.get('/:Search',(req,res) => {
 
     const buscaPeça = req.params.Search
-    console.log('aqui ta minha ' +buscaPeça)
-    
-    
     try{
-
-
-        /*if(!buscaPeça){
-            return res.render('admin/home',{produto:[]})
-        }*/
+        if(buscaPeça === 'imagem.ico'){
+            return res.render('admin/home')
+        }
          Produto.findAll({
             where: {
                 produto:{
@@ -75,6 +70,72 @@ app.get('/:Search',(req,res) => {
     }catch (err) {
         console.error('Erro inesperado:', err);
         res.status(500).send('Erro inesperado');
+    }
+})
+
+/*app.get('/addpeca/estoque',(req,res) => {
+    res.render('addpeca/estoque')
+})*/
+app.get('/addpeca/estoque', (req,res) => {
+    
+    const buscaCodigo = req.query.buscaCodigo
+    
+    try{
+        Produto.findOne({
+            where:{
+                codigo:{
+                    [Op.eq]: buscaCodigo
+                }
+            }
+        }).then((produto) => {
+            res.render('addpeca/estoque',{produto: produto} )
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    }catch(err) {
+    console.log(err)
+    }
+})
+app.post('/addpeca/estoque/u', (req,res) => {
+    const buscaCodigo = req.body.cod
+    const updateQuantidade = parseInt(req.body.addQuantidade)
+    const updateValorC = parseFloat(req.body.addValorC)
+    const updateValorV = parseFloat(req.body.addValorV)
+
+    try{
+        Produto.findOne({
+            whre: {
+                codigo: {
+                    [Op.eq]: buscaCodigo
+                }
+            }
+        }).then((produto) => {
+            if(produto){
+
+                const novaquantidade = updateQuantidade + produto.quantidade;
+                let novoValoC
+                if(produto.quantidade > 0){
+                    const mult = (produto.quantidade*produto.valorc)+(updateQuantidade*updateValorC)
+                     novoValoC = mult/novaquantidade
+                }
+
+                produto.update({
+                    quantidade: novaquantidade,
+                    valorc: novoValoC,
+                    valorv: updateValorV
+                })
+                .then(() => {
+                    res.render('addpeca/estoque')
+                }).catch((err) => {
+                    console.log('Erro ao atualizar o produto:', err);
+                    res.status(500).send('Erro ao atualizar o produto.');
+                });
+            }
+        })
+    }catch (err) {
+        console.log('Erro no servidor:', err);
+        res.status(500).send('Erro no servidor.');
     }
 })
 app.get('/',(req,res) => {
